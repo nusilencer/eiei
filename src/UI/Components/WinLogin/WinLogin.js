2fe3b44b5aad9bb8774d3bda33a1b2e44ed627c1
nusilencer <39714272+nusilencer@users.noreply.github.com>
2025-05-19 20:00:30 +0700

    Add files via upload

---
+++ src/UI/Components/WinLogin/WinLogin.js
@@ -0,0 +1,36 @@
+/**
+ * UI/Components/WinLogin/WinLogin.js
+ *
+ * Login Window
+ *
+ * This file is part of ROBrowser, (http://www.robrowser.com/).
+ *
+ */
+define(function (require)
+{
+	'use strict';
+	
+	var publicName = 'WinLogin';
+	
+	var WinLogin = require('./WinLogin/WinLogin');
+	var WinLoginV2 = require('./WinLoginV2/WinLoginV2');
+	
+	var UIVersionManager = require('UI/UIVersionManager');
+	
+	var versionInfo = {
+		default: WinLogin,
+		common: {
+			20221114:	WinLogin
+		},
+		re: {
+			
+		},
+		prere:{
+			
+		}
+	};
+	
+	var Controller = UIVersionManager.getUIController(publicName, versionInfo);
+	
+	return Controller;
+});
---
+++ src/UI/Components/WinStats/WinStats.js
@@ -0,0 +1,42 @@
+/**
+ * UI/Components/WinStats/WinStats.js
+ *
+ * Chararacter Basic information windows
+ *
+ * Note: For different versions, please use different Object names and main div IDs to avoid conflicts in settings and styles
+ *
+ * This file is part of ROBrowser, (http://www.robrowser.com/).
+ *
+ */
+define(function (require)
+{
+	'use strict';
+	
+	var publicName = 'WinStats';
+	
+	var WinStats = require('./WinStats/WinStats');
+	var WinStatsV1 = require('./WinStatsV1/WinStatsV1');
+	var WinStatsV2 = require('./WinStatsV2/WinStatsV2');
+	var WinStatsV3 = require('./WinStatsV3/WinStatsV3');
+	
+	var UIVersionManager = require('UI/UIVersionManager');
+	
+	var versionInfo = {
+		default: WinStats,
+		common: {
+			20220520:	WinStatsV3,
+			20140521:	WinStatsV2,
+			20090617:	WinStatsV1,
+		},
+		re: {
+			
+		},
+		prere:{
+			
+		}
+	};
+	
+	var WinStatsController = UIVersionManager.getUIController(publicName, versionInfo);
+	
+	return WinStatsController;
+});
